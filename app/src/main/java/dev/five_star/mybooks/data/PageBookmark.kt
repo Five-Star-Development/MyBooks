77bd0fcafeebad39a9b1de84dbe1006982ae28c2
Benny Özcetin <benny@five-star.dev>
2022-01-03 18:55:47 +0100

    add ignored files

---
+++ app/src/main/java/dev/five_star/mybooks/data/PageBookmark.kt
@@ -0,0 +1,13 @@
+package dev.five_star.mybooks.data
+
+import android.os.Parcelable
+import dev.five_star.mybooks.database.PageEntry
+import kotlinx.parcelize.Parcelize
+import java.util.*
+
+@Parcelize
+data class PageBookmark(val date: Date, val page: Int) : Parcelable
+
+fun PageEntry.toPageBookmark(): PageBookmark {
+    return PageBookmark(date = date, page = page)
+}
