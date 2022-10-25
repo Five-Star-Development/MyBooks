9c606492d21bc3c6fce20049386efb2de0f6e71f
Benny Özcetin <benny@five-star.dev>
2022-10-26 00:09:04 +0200

    add archive tab with button bar - maybe just a menu instead

--- app/src/main/java/dev/five_star/mybooks/MainActivity.kt
+++ app/src/main/java/dev/five_star/mybooks/MainActivity.kt
@@ -2,6 +2,9 @@ package dev.five_star.mybooks
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -10,5 +13,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        val navView: BottomNavigationView = findViewById(R.id.bottom_navigation)
+        navView.setupWithNavController(navController)
+
     }
 }
